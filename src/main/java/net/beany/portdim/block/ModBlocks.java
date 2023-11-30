f4ed1ffd41cc432a3083ad065cd2bccffd535936
harry <harry.knapp147@gmail.com>
2023-11-30 19:51:50 +0000

    yes

--- src/main/java/net/beany/portdim/block/ModBlocks.java
+++ src/main/java/net/beany/portdim/block/ModBlocks.java
@@ -1,21 +1,28 @@
 package net.beany.portdim.block;
 
 import net.beany.portdim.PortDim;
+import net.beany.portdim.block.custom.BlueSpaceInserter;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
+import net.minecraft.client.sound.Sound;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block BLUESPACE_BLOCK = registerBlock("bluespace_block",
-            new Block(FabricBlockSettings.copyOf(Blocks.ANDESITE)));
+            new BlueSpaceInserter(FabricBlockSettings.copyOf(Blocks.DIRT).sounds(BlockSoundGroup.STONE)));
     public static final Block BLUESPACE_ORE = registerBlock("bluespace_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_ORE)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE).strength(2f), UniformIntProvider.create(4, 7)));
+
+
 
     private static Block registerBlock(String name,Block block) {
         registerBlockItem(name, block);
